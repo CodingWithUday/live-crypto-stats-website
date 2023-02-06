86a77b869c1819da9313e7ca6150951dc51e4d4e
Uday Prakash Gupta <70849646+CodingWithUday@users.noreply.github.com>
2023-02-06 19:48:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# live-crypto-stats-website
+just made for fun (note: NOW WITH CSS!!!!!)
+
+
+
+
+
+you can check the website here - https://codingwithuday.github.io/live-crypto-stats-website/
---
+++ redirect-app.js
@@ -0,0 +1,4 @@
+document.getElementById("cryptoBtn").onclick = function(){
+    var cryppNme = document.getElementById("cryptoName").value;
+    window.location.href = "https://codingwithuday.github.io/live-crypto-stats-website/crypto.html?crypto="+cryppNme; // add your website link instead of mines here :)
+}
